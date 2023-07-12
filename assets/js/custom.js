8c980d3cbe23b44b9f5334d9eda479555649a09a
steveaq <47524231+steveaq@users.noreply.github.com>
2023-07-12 04:07:35 +0100

    toc floating

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,11 @@
 window.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggle-toc");
+    const tocContent = document.getElementById("toc-content");
+  
+    toggleButton.addEventListener("click", function() {
+      tocContent.classList.toggle("hidden");
+    });
+  
     const tocWrapper = document.getElementById("toc-wrapper");
     if (tocWrapper) {
       const offsetTop = tocWrapper.offsetTop;
